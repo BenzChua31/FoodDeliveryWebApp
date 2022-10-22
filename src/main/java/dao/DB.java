c5bd7188782e3f69463c3a706a5adccf32b89e0d
BenzChua31 <benzchua31@gmail.com>
2022-10-23 00:28:45 +1100

    Updated db.java

--- src/main/java/dao/DB.java
+++ src/main/java/dao/DB.java
@@ -7,10 +7,10 @@ public class DB {
 
     // Benz's DB URL --- jdbc:mysql://localhost:3306, db, root, root,
     // com.mysql.cj.jdbc.Driver
-    protected String URL = "jdbc:mysql://fds10sql.mysql.database.azure.com:3306/";
+    protected String URL = "jdbc:mysql://fdssql.mysql.database.azure.com:3306/";
     protected String db = "db?useSSL=true";
     protected String dbuser = "g10";
-    protected String dbpass = "Root!Root!";
+    protected String dbpass = "ro0T!ro0T!";
     protected String driver = "com.mysql.cj.jdbc.Driver";
     protected Connection conn;
 }
